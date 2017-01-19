ec152e981a229d9edee7142343881bc7d01ad973
Joe Karlsson <joe.carlson@callmejoe.net>
2017-01-18 15:12:23 -1000

    can move DL sprite with arrow keys now

--- src/states/Game.js
+++ src/states/Game.js
@@ -25,11 +25,29 @@ export default class extends Phaser.State {
     })
 
     this.game.add.existing(this.devLeagueLogo)
+
+    //  In this example we'll create 4 specific keys (up, down, left, right) and monitor them in our update function
+    this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP)
+    this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
+    this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+    this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
   }
 
   render () {
     if (__DEV__) {
       this.game.debug.spriteInfo(this.devLeagueLogo, 32, 32)
+    }
+
+    if (this.upKey.isDown) {
+      this.devLeagueLogo.y--
+    } else if (this.downKey.isDown) {
+      this.devLeagueLogo.y++
+    }
+
+    if (this.leftKey.isDown) {
+      this.devLeagueLogo.x--
+    } else if (this.rightKey.isDown) {
+      this.devLeagueLogo.x++
     }
   }
 }
